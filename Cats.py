d15172b40962d3a6e7094b3bd16c75dfd4296d76
Андрей Стародедов <119430273+AndrewStar89@users.noreply.github.com>
2023-03-27 14:18:06 +0300

    Cats.py

---
+++ Cats.py
@@ -0,0 +1,7 @@
+from CatCat import Pets
+
+cat1 = Pets('Барон', 'Мальчик', 2)
+cat2 = Pets('Сэм', 'Мальчик', 2)
+
+print('Имя первого кота - ', cat1.name, '- Его пол - ', cat1.gender, '- Его возраст - ', cat1.age)
+print('Имя второго кота - ', cat2.name, '- Его пол - ', cat2.gender, '- Его возраст - ', cat2.age)
